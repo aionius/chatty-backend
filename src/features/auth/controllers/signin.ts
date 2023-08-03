6a21d9b218b21abfbd2e5c3969286f3af4aea843
Aionius Dactlys <aionius@gmail.com>
2023-08-03 12:35:11 -0400

    feature: implemented password reset feature with unit test

--- src/features/auth/controllers/signin.ts
+++ src/features/auth/controllers/signin.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import JWT from 'jsonwebtoken';
 import HTTP_STATUS from 'http-status-codes';
+// import moment from 'moment';
+// import publicIp from 'ip';
 
 import { config } from '@root/config';
 import { joiValidation } from '@globals/decorators/joi-decorators';
@@ -10,6 +12,10 @@ import { authService } from '@service/db/auth.service';
 import { BadRequestError } from '@globals/helpers/error-handler';
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { userService } from '@service/db/user.service';
+// import { emailQueue } from '@service/queues/email.queue';
+// import { forgotPasswordTemplate } from '@service/emails/templates/forgot-password/forgot-password.template';
+// import { resetPasswordTemplate } from '@service/emails/templates/reset-password/reset-password.template';
+// import { mailTransport } from '@service/emails/mail.transport';
 
 export class SignIn {
   @joiValidation(loginSchema)
@@ -40,6 +46,24 @@ export class SignIn {
       },
       config.JWT_TOKEN!
     );
+
+    // sending email
+    // await mailTransport.sendEmail('mireille52@ethereal.email', 'Testing Development Email', 'This is a test email.');
+
+    // password change confirmation email
+    // const templateParams: IResetPasswordParams = {
+    //   username: existingUser.username!,
+    //   email: existingUser.email!,
+    //   ipaddress: publicIp.address(),
+    //   date: moment().format('DD/MM/YYY HH:mm')
+    // };
+    // const template: string = resetPasswordTemplate.passwordResetConfirmationTemplate(templateParams);
+    // emailQueue.addEmailJob('forgotPasswordEmail', { template, receiverEmail: 'mireille52@ethereal.email', subject: 'Password reset confirmation' });
+
+    // forgot password email
+    // const resetLink = `${config.CLIENT_URL}/reset-password?token=123412341234`;
+    // const template: string = forgotPasswordTemplate.passwordResetTemplate(existingUser.username!, resetLink);
+    // emailQueue.addEmailJob('forgotPasswordEmail', { template, receiverEmail: 'mireille52@ethereal.email', subject: 'Reset your password' });
 
     req.session = { jwt: userJWT };
 
